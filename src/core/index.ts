3a9c181afd758ace7ed22128bc154e9beb48a818
StijnVerhelst <stijn09.verhelst@gmail.com>
2026-01-17 19:47:58 +0100

    Initial commit

--- src/core/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Core module exports
- */
-
-export { NunjucksRenderer, nunjucksRenderer } from "./NunjucksRenderer";
-export { applyMappings, applyMappingsToTables } from "./MappingProcessor";
-export { DataOrchestrator, dataOrchestrator } from "./DataOrchestrator";
--- src/services/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Service exports
- */
-
-export { TableService, tableService, type TableInfo } from "./TableService";
-export { GlobalsService, globalsService, type NamedRangeInfo } from "./GlobalsService";
-export { OutputService, outputService } from "./OutputService";
